409b597e6319751819311e96ab5e39887cfb3a9f
Yu Chieh Lin <jessicalin917@gmail.com>
2019-01-29 14:17:28 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Yu Chieh Lin on 1/29/19.
+//  Copyright © 2019 Yu Chieh Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
