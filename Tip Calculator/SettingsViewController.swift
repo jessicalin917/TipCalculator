622454c262f2efb887e50238c0110fd7a13417e9
Yu Chieh Lin <jessicalin917@gmail.com>
2019-01-30 23:09:39 -0800

    updated functionality - sliders and settings

--- Tip Calculator/SettingsViewController.swift
+++ Tip Calculator/SettingsViewController.swift
@@ -11,60 +11,79 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var tip1Label: UILabel!
+    @IBOutlet weak var tip2Label: UILabel!
+    @IBOutlet weak var tip3Label: UILabel!
     
-    // @IBOutlet weak var defaultTips: UISegmentedControl!
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    @IBOutlet weak var slider3: UISlider!
     
-    // Default tip percentages that are initially set
-    var tipPercentages = [0.15, 0.18, 0.2]
+    // default value array
+    var tipPercentages = [0.15, 0.18, 0.20]
     
-    // Default tip percentage that is currently being used
-    var tipPercent = 0.0
+    // slider defaults
+    let default1 = 15
+    let default2 = 18
+    let default3 = 20
     
-    // Default tip values
-    let defaultTip1 = 0.15
-    let defaultTip2 = 0.18
-    let defaultTip3 = 0.20
-    
+    let defaults = UserDefaults.standard
+    // set strings for tip percentages
     let customTip1 = "customTip1"
     let customTip2 = "customTip2"
     let customTip3 = "customTip3"
+    // set strings for slider positions
+    let sliderPos1 = "sliderPos1"
+    let sliderPos2 = "sliderPos2"
+    let sliderPos3 = "sliderPos3"
     
-    let tipPercentArray = "tipPercentArray"
-    
-    // Used to save and load saved values
-    let defaults = UserDefaults.standard
-    
-    
-    @IBAction func changeTip1Slider(_ sender: UISlider) {
-        // get value
-        let value = sender.value
-        
-        // change label
-        tip1Label.text = String(format: "%.0f", value) + "%"
+    @IBAction func changeTip1(_ sender: UISlider) {
+        // update label
+        tip1Label.text = String(format: "%.0f", sender.value) + "%"
         
         // update defaults
-        defaults.set(Double(value), forKey: customTip1)
-        setPercent(percent: Double(value), index: 0);
-    }
-    
-    func setPercent( percent: Double, index: Int) {
-        // change tip value of segmented control
-        // defaultTips.setTitle((String(format: "%.0f", percent) + "%"), forSegmentAt: index)
-        
-        // calculate tip decimal
-        let tipPercent = percent / 100
-        
-        // update array
-        tipPercentages[index] = tipPercent
-        
-        // update defaults array
-        defaults.set(tipPercentages, forKey: tipPercentArray)
+        defaults.set(sender.value, forKey: sliderPos1)
+        customizeTip( percent: Double(sender.value), index: 0 )
         defaults.synchronize()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        loadTips()
+    @IBAction func changeTip2(_ sender: UISlider) {
+        // update label
+        tip2Label.text = String(format: "%.0f", sender.value) + "%"
+        
+        // update defaults
+        defaults.set(sender.value, forKey: sliderPos2)
+        customizeTip( percent: Double(sender.value), index: 1 )
+        defaults.synchronize()
+    }
+    
+    @IBAction func changeTip3(_ sender: UISlider) {
+        // update label
+        tip3Label.text = String(format: "%.0f", sender.value) + "%"
+        
+        // update defaults
+        defaults.set(sender.value, forKey: sliderPos3)
+        customizeTip( percent: Double(sender.value), index: 2 )
+        defaults.synchronize()
+    }
+    
+    func customizeTip( percent: Double, index: Int ) {
+        var currentTip: String = ""
+        
+        // specify string
+        if index == 0 {
+            currentTip  = customTip1
+        } else if index == 1 {
+            currentTip = customTip2
+        } else if index == 2 {
+            currentTip = customTip3
+        }
+        
+        // update tip array
+        let newTip = percent / 100
+        tipPercentages[index] = newTip
+        
+        // updates defaults
+        defaults.set(newTip, forKey: currentTip)
     }
     
     override func viewDidLoad() {
@@ -73,25 +92,61 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func loadTips() {
-        var tip1 = tipPercentages[0]
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadDefaults()
+    }
+    
+    func loadDefaults() {
+        // load the defaults' tips
+        var tip1 = defaults.double(forKey: customTip1)
+        var tip2 = defaults.double(forKey: customTip2)
+        var tip3 = defaults.double(forKey: customTip3)
         
-        if var tip1 = defaults.object(forKey: "customTip1") as? Double {
-            tip1 = defaults.double(forKey: customTip1)
+        // if null, set them to defaults
+        if tip1 == 0 {
+            tip1 = tipPercentages[0]
         }
         
+        if tip2 == 0 {
+            tip2 = tipPercentages[1]
+        }
+        
+        if tip3 == 0 {
+            tip3 = tipPercentages[2]
+        }
         
         // update array
-        tipPercentages[0] = tip1 / 100
+        tipPercentages[0] = tip1
+        tipPercentages[1] = tip2
+        tipPercentages[2] = tip3
         
-        // set segment control
-        // defaultTips.setTitle((String(format: "%.0f", tip1) + "%"), forSegmentAt: 0)
+        // set tip labels to value
+        tip1Label.text = String(format: "%.0f", (tip1 * 100)) + "%"
+        tip2Label.text = String(format: "%.0f", (tip2 * 100)) + "%"
+        tip3Label.text = String(format: "%.0f", (tip3 * 100)) + "%"
         
-        // set text fields
-        tip1Label.text = String(format: "%.0f" + "%", tip1)
+        
+        var sliderVal1 = defaults.float(forKey: sliderPos1)
+        var sliderVal2 = defaults.float(forKey: sliderPos2)
+        var sliderVal3 = defaults.float(forKey: sliderPos3)
+        
+        if sliderVal1 == 0 {
+            sliderVal1 = Float(default1)
+        }
+        if sliderVal2 == 0 {
+            sliderVal2 = Float(default2)
+        }
+        if sliderVal3 == 0 {
+            sliderVal3 = Float(default3)
+        }
+        
+        // set slider values
+        slider1.value = sliderVal1
+        slider2.value = sliderVal2
+        slider3.value = sliderVal3
         
     }
-
     
 
     /*
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -15,9 +15,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    // default value array
+    var tipPercentages = [0.15, 0.18, 0.20]
+    
+    // slider defaults
+    let default1 = 15
+    let default2 = 18
+    let default3 = 20
+    
+    let defaults = UserDefaults.standard
+    // set strings for tip percentages
+    let customTip1 = "customTip1"
+    let customTip2 = "customTip2"
+    let customTip3 = "customTip3"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.endEditing(true)
+        loadDefaults()
     }
    
     @IBAction func onTap(_ sender: Any) {
@@ -29,9 +48,6 @@ class ViewController: UIViewController {
         // Get the bill count
         let bill = Double(billField.text!) ?? 0
         
-        // Array of percentages
-        let tipPercentages = [0.15, 0.18, 0.20]
-        
         // Calculate tip and total
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
@@ -39,7 +55,37 @@ class ViewController: UIViewController {
         // Update tip and total 
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+    }
     
+    func loadDefaults() {
+        // load the defaults' tips
+        var tip1 = defaults.double(forKey: customTip1)
+        var tip2 = defaults.double(forKey: customTip2)
+        var tip3 = defaults.double(forKey: customTip3)
+        
+        // if null, set them to defaults
+        if tip1 == 0 {
+            tip1 = tipPercentages[0]
+        }
+        
+        if tip2 == 0 {
+            tip2 = tipPercentages[1]
+        }
+        
+        if tip3 == 0 {
+            tip3 = tipPercentages[2]
+        }
+        
+        // update array
+        tipPercentages[0] = tip1
+        tipPercentages[1] = tip2
+        tipPercentages[2] = tip3
+        
+        // set tip control
+        tipControl.setTitle(String(format: "%.0f", (tip1 * 100)) + "%", forSegmentAt: 0)
+        tipControl.setTitle(String(format: "%.0f", (tip2 * 100)) + "%", forSegmentAt: 1)
+        tipControl.setTitle(String(format: "%.0f", (tip3 * 100)) + "%", forSegmentAt: 2)
+        
     }
 }
 
